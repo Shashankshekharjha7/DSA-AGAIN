86c952d25946417570350c3499bb94f389ad1c94
Shashankshekharjha7 <shashankshekharjha7@gmail.com>
2026-07-27 19:12:10 +0530

    8th pattern question

---
+++ .history/008_Rhombus_pattern_20260727190947.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void printPattern(int n){
+  //outer loop
+  for(int i = 1; i<=n; i++){
+    //inner loop(spaces)
+    for(int j = 1; j<=n-i; j++){
+      cout << " ";
+    }
+    
+  }
+}
