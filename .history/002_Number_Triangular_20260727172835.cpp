725442714fbff8b27f97876b0a48b6a921b5a9e6
Shashankshekharjha7 <shashankshekharjha7@gmail.com>
2026-07-27 17:33:48 +0530

    pattern_question_2

---
+++ .history/002_Number_Triangular_20260727172835.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void printPattern(int n){
+  //outer loop to handle rows
+  for(int i = 1; i <= n; i++){
+    
+    //Inner loop to print leading spaces
+    for(int j = 1; j<=n-i; j++)
+  }
+}
