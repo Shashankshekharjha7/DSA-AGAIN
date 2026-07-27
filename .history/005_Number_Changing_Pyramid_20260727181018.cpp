7b66c2acffeffd8ce0d77c47442045870817c2e5
Shashankshekharjha7 <shashankshekharjha7@gmail.com>
2026-07-27 18:14:16 +0530

    5th question pattern

---
+++ .history/005_Number_Changing_Pyramid_20260727181018.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printPattern(){
+  int i,j;
+  int num = 1;
+
+  for(i = )
+}
---
+++ .history/005_Number_Changing_Pyramid_20260727181053.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printPattern(){
+  int i,j;
+  int num = 1;
+
+  for(i = 1; i<=n; i++){
+    for(j = 1;j<)
+  }
+}
